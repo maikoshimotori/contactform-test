857d9b08781d130c66cafab5789c314a6bdf6f84
maiko <3430maiko@gmail.com>
2024-11-15 21:03:41 +0900

    サンプルファイルの追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,5 +15,6 @@ use App\Http\Controllers\ContactController;
 */
 
 Route::get('/', [ContactController::class, 'index']);
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
 Route::post('/contacts', [ContactController::class, 'store']);
